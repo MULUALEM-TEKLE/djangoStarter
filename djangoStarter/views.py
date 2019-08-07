73bbbb76b1a0747064904a3b5438295cb0b69ba4
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-08-07 11:22:12 +0300

    Created view

--- djangoStarter/views.py
+++ djangoStarter/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.views.generic import TemplateView
+from djangoStarter.models import Post
 
 # Create your views here.
+
+class post_feed(TemplateView):
+    template_name = 'index.html'
+
+def get_context_data(self, *args , **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["posts"] = Post.objects.all() 
+    return context
+

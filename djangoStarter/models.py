a617331c657f895850f55c270b0d46c55341ad4b
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-08-07 19:01:39 +0300

    Tried to fix the error with the model and added vscode dir to the gitignore file

--- djangoStarter/models.py
+++ djangoStarter/models.py
@@ -3,15 +3,15 @@ import datetime
 
 # Create your models here.
 class Post(models.Model):
+
     objects = models.Manager()
 
     image = models.FileField(upload_to='images/')
     title = models.CharField(default="" , max_length=64)
     description = models.CharField(default="" , max_length=512)
     date = models.DateField(default=datetime.date.today)
-
-    class Meta:
-        ordering = ['-date']
+    # class Meta:
+    #     ordering = ['-date']
     
     def __str__(self):
         return self.title
